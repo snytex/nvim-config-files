39610a27cb6e8b616ca23386103c7824257be7da
Snytex <61839845+snytex@users.noreply.github.com>
2026-03-15 22:35:01 +0100

    Update alpha.lua
    Removed the Projects button from the dashboard.

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -29,11 +29,6 @@ return {
 			dashboard.button("n", "  New file", ":enew <BAR> startinsert <CR>"),
 			dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 			dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
-			dashboard.button(
-				"p",
-				"  Projects",
-				':lua require("telescope.pickers").new({}, { prompt_title = "📁 Projekte", finder = require("telescope.finders").new_table({ results = vim.fn.systemlist("find ~/Projekte -maxdepth 1 -mindepth 1 -type d"), entry_maker = function(e) return { value = e, display = vim.fn.fnamemodify(e, ":t"), ordinal = vim.fn.fnamemodify(e, ":t"), path = e } end }), sorter = require("telescope.config").values.generic_sorter({}), attach_mappings = function(b, m) require("telescope.actions").select_default:replace(function() local s = require("telescope.actions.state").get_selected_entry() require("telescope.actions").close(b) vim.cmd("cd " .. s.path) vim.cmd("edit .") end) return true end }):find()<CR>'
-			),
 			dashboard.button("g", "  Find text", ":Telescope live_grep <CR>"),
 			dashboard.button("l", "  Lazy", ":Lazy<CR>"),
 			dashboard.button("q", "  Quit", ":qa<CR>"),
